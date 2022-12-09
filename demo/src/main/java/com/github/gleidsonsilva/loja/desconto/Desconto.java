1e90d85ead30095cb3d82592122f097234ac0769
Gleidson Silva <gledson.leytte@hotmail.com>
2022-12-08 23:39:09 -0300

    Using template method

--- demo/src/main/java/com/github/gleidsonsilva/loja/desconto/Desconto.java
+++ demo/src/main/java/com/github/gleidsonsilva/loja/desconto/Desconto.java
@@ -6,8 +6,17 @@ import com.github.gleidsonsilva.loja.orcamento.Orcamento;
 
 public abstract class Desconto {
     protected Desconto proximo;
+
     public Desconto(Desconto proximo) {
         this.proximo = proximo;
     }
-    public abstract BigDecimal calcular(Orcamento orcamento);
+
+    public BigDecimal calcular(Orcamento orcamento) {
+        return deveAplicar(orcamento) ? efetuarCalculo(orcamento)
+                : proximo.calcular(orcamento);
+    }
+
+    protected abstract BigDecimal efetuarCalculo(Orcamento orcamento);
+
+    public abstract boolean deveAplicar(Orcamento orcamento);
 }
